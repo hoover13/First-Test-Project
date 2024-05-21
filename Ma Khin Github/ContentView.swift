a3202e87e6d3419697ecc149f088103f9e6c5a62
hoover13 <hoovermtk25@gmail.com>
2024-05-21 11:45:05 -0400

    Initial Commit

---
+++ Ma Khin Github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ma Khin Github
+//
+//  Created by Min Thu Khine on 5/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ma Khin Github/Ma_Khin_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ma_Khin_GithubApp.swift
+//  Ma Khin Github
+//
+//  Created by Min Thu Khine on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Ma_Khin_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
